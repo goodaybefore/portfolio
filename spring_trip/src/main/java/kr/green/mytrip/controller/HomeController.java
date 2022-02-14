35e0bb0dc3f228387031396179e5f2d895d60050
goodaybefore <goodaybefore@gmail.com>
2022-02-14 17:51:22 +0900

    포폴 : tiles 적용중

---
+++ spring_trip/src/main/java/kr/green/mytrip/controller/HomeController.java
@@ -0,0 +1,22 @@
+package kr.green.mytrip.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(ModelAndView mv) {
+		
+		mv.setViewName("home");
+		return mv;
+	}
+	
+}
