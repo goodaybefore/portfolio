b08d1ce80de3ce635ab0128973db247b46fe7abd
goodaybefore <goodaybefore@gmail.com>
2022-03-14 17:53:53 +0900

    포폴 : SQL 업데이트
    - ac_sort_name 추가
    *아마 집가서 ac_mca_name, ca_sca_name 추가해야하하지 않을까..? trip처럼..

---
+++ sql/2022-03-14/my_trip_site_activity.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_trip_site
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activity`
+--
+
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activity` (
+  `ac_num` int NOT NULL AUTO_INCREMENT,
+  `ac_tr_num` int NOT NULL,
+  `ac_title` varchar(50) DEFAULT NULL,
+  `ac_sort_name` varchar(45) DEFAULT NULL,
+  `ac_address` varchar(50) DEFAULT NULL,
+  `ac_date` date DEFAULT NULL,
+  `ac_contents` longtext,
+  `ac_color` varchar(10) DEFAULT NULL,
+  `ac_op_name` varchar(10) NOT NULL,
+  `ac_del` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`ac_num`),
+  KEY `FK_trip_TO_activity_1` (`ac_tr_num`),
+  KEY `FK_open_range_TO_activity_1_idx` (`ac_op_name`),
+  CONSTRAINT `FK_open_range_TO_activity_1` FOREIGN KEY (`ac_op_name`) REFERENCES `open_range` (`op_name`),
+  CONSTRAINT `FK_trip_TO_activity_1` FOREIGN KEY (`ac_tr_num`) REFERENCES `trip` (`tr_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activity`
+--
+
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-14 17:52:59
