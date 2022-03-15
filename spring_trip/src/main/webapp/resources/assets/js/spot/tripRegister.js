65d70dcf181eae151e4561e33e484d9e33695d7a
goodaybefore <goodaybefore@gmail.com>
2022-03-15 15:49:54 +0900

    포폴 : tripReg페이지 수정
    - {spot_user}에 맞게 수정해줌

--- spring_trip/src/main/webapp/resources/assets/js/spot/tripRegister.js
+++ spring_trip/src/main/webapp/resources/assets/js/spot/tripRegister.js
@@ -2,60 +2,6 @@
  * 
  */
 $(function() {
-	setMiddleCategory();
-	$('.middle-category').change(function(){
-		let mc_num = $(this).val();
-		let middle_ca_str = '<input type="hidden" name="mc_num" value="'+mc_num+'">'; 
-		setSmallCategory(mc_num);
-		$('.area-select-mcnum').html(middle_ca_str);
-	})
-	
-	
-	$('.small-category').change(function(){
-		let sc_num = $(this).val();
-		let small_ca_str = '<input type="hidden" name="sc_num" value="'+sc_num+'">'; 
-		$('.area-select-scnum').html(small_ca_str);
-		//setSmallCategory(sc_num);
-	})
-	
-	function setMiddleCategory(){
-		let str = '<option value="0">시/도선택</option>';
-		$.ajax({
-				async :false,
-		    type:'get',
-		    url : '/spot/middlecategory',
-		    dataType:"json",
-		    success : function(res){
-		    	for(middle of res.list){
-		    		str += '<option value="'+middle.mc_num+'">'+middle.mc_name+'</option>';
-		    	}
-		    	$('.middle-category').html(str);
-		    	}
-		})
-	}
-	
-	function setSmallCategory(mc_num){
-		let str = '<option value="0">세부선택</option>';
-		if(mc_num<=0){
-			$('.middle-category').html(str);
-			return;
-		}
-		$.ajax({
-				async :false,
-		    type:'get',
-		    url : '/spot/smallcategory?sc_mc_num='+mc_num,
-		    dataType:"json",
-		    success : function(res){
-		    	for(small of res.list){
-		    		str += '<option value="'+small.sc_num+'">'+small.sc_name+'</option>';
-		    	}
-		    	$('.small-category').html(str);
-		    	}
-		})
-	}
-	
-			
-			
 	$('#from').daterangepicker({
 	    "locale": {
 	        "format": "YYYY-MM-DD",
