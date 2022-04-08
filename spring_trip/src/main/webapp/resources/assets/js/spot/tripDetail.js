ec5c0dfab959ecde2f823e50d23ae4328b9d6455
goodaybefore <goodaybefore@gmail.com>
2022-04-08 13:04:35 +0900

    Update tripDetail.js

--- spring_trip/src/main/webapp/resources/assets/js/spot/tripDetail.js
+++ spring_trip/src/main/webapp/resources/assets/js/spot/tripDetail.js
@@ -19,6 +19,44 @@ $(function() {
 	}
 	
 	
-	
-	
+	function showDbMarkers(){
+		
+				showDefaultMap();
+				
+			//주소를 좌표로 변환하는 코드
+			var callback = function(result, status){
+				if(status ==kakao.maps.services.Status.OK){
+					coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+					coordList.push(coords);
+				}
+			}
+			
+			
+			for(i=0;i<address_list.length;i++){
+				geocoder.addressSearch(address_list[i], callback);
+			}
+			
+			//배열에 들어간 coordList 사용하기
+			setTimeout(function(){
+				console.log(coordList);
+				
+				var marker;
+				for(var i=0;i<coordList.length;i++){
+					marker = new kakao.maps.Marker({position : coordList[i]})
+					marker.setMap(map);
+					bounds.extend(coordList[i]);
+				}
+				$('#btnMaps').click();
+			}, 200);
+			}
+			
+			//기본지도 출력
+			function showDefaultMap(){
+				var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+		    	mapOption = {
+						center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		      	level: 3 // 지도의 확대 레벨
+		    	};
+				map = new kakao.maps.Map(mapContainer, mapOption);
+			}
 });
